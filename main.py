14a9f3cf91756d41f2be673ba1f107f9f125f120
ericlovesmath <ericlovesmath@gmail.com>
2024-05-07 18:33:12 -0700

    Generated points for two faces, use native Delauney to test triangulation

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib.backend_bases import MouseButton
-from scipy.spatial import Delaunay
 from PIL import Image
+from scipy.spatial import Delaunay
 
 # --------------------------------------------------------------------------------------
 
 
 def read_image(path: str) -> np.ndarray:
     """Read an image an array of linear RGB radiance values ∈ [0,1]"""
-    return (np.array(Image.open(path), dtype=np.float32) / 255) ** 2.2
+    return np.array(Image.open(path), dtype=np.float32) / 255
 
 
 def write_image(path: str, img: np.ndarray) -> None:
@@ -27,7 +27,7 @@ def write_gif(path: str, imgs: List[np.ndarray]) -> None:
 
 
 def numpy_to_image(img: np.ndarray) -> Image.Image:
-    return Image.fromarray(np.uint8(255 * img.clip(0, 1) ** (1 / 2.2)))
+    return Image.fromarray(np.uint8(255 * img.clip(0, 1)))
 
 
 def resize(img: np.ndarray, size: Tuple[int, int]) -> np.ndarray:
@@ -84,11 +84,43 @@ def run_naive_cross_fade():
     write_image("out/cross_fade.png", cross_dissolve(menzel, muscato, 0.5))
 
 
-def run_draw_points():
-    menzel = resize(read_image("imgs/idina_menzel.png"), (1000, 1000))
-    select_points("points/idina_menzel.csv", menzel)
+def run_draw_points(fname: str):
+    img = resize(read_image(f"imgs/{fname}.png"), (1000, 1000))
+    select_points(f"points/{fname}.csv", img)
 
+
+def run_draw_mesh(fname: str):
+    img = resize(read_image(f"imgs/{fname}.png"), (1000, 1000))
+    points = pd.read_csv(f"points/{fname}.csv", header=None).to_numpy()
+
+    border = []
+    for i in np.linspace(0, 1000, 9):
+        border.extend([[0, i], [1000, 1000 - i], [1000 - i, 0], [i, 1000]])
+    points = np.vstack([points, np.array(border)])
+
+    triangles = Delaunay(points)
+
+    plt.imshow(numpy_to_image(img))
+    plt.scatter(points[:, 0], points[:, 1])
+    plt.triplot(points[:, 0], points[:, 1], triangles.simplices, linewidth=1)
+
+    plt.axis((0, 1000, 1000, 0))
+    plt.savefig(f"out/{fname}_mesh.png")
+    plt.show()
+
+def run_verify_points():
+    plt.axis((0, 1000, 1000, 0))
+    points1 = pd.read_csv(f"points/idina_menzel.csv", header=None).to_numpy()
+    points2 = pd.read_csv(f"points/jamie_muscato.csv", header=None).to_numpy()
+    for p1, p2 in zip(points1, points2):
+        plt.plot(p1[0], p1[1], "bo", ms=2)
+        plt.plot(p2[0], p2[1], "ro", ms=2)
+        plt.pause(0.2)
 
 if __name__ == "__main__":
     # run_naive_cross_fade()
-    run_draw_points()
+    # run_draw_points("idina_menzel")
+    # run_draw_points("jamie_muscato")
+    # run_draw_mesh("idina_menzel")
+    # run_draw_mesh("jamie_muscato")
+    run_verify_points()
